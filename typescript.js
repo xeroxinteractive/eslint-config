e3d15fd0fd5005cd70f9cd249d65368656a7452e
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-10-07 18:54:36 +0100

    fix(typescript): check jsdoc examples with TS

--- typescript.js
+++ typescript.js
@@ -7,6 +7,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
+    extraFileExtensions: ['<text>'],
   },
   rules: {
     '@typescript-eslint/camelcase': 'off',
